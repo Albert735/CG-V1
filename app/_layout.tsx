794b4b70b86a6399e607a36bea23a1dfbbe56288
Albert735 <03jayteee@gmail.com>
2026-02-25 01:08:46 +0000

    feat: Implement tab-based navigation with new screens, update TypeScript configuration, and remove the license file.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { LogBox } from "react-native";
+
+
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
